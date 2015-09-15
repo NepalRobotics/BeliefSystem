96a1dc6e887d29102e37737de89eaee8cd4ee5a6
Daniel Petti <djpetti@gmail.com>
2015-09-14 20:46:41 -0400

    Write a test for the kalman filter.

--- kalman.py
+++ kalman.py
@@ -5,7 +5,9 @@ import numpy as np
 
 from pykalman import AdditiveUnscentedKalmanFilter
 
+
 logger = logging.getLogger(__name__)
+
 
 """ Handles a Kalman filter for computing drone and transmitter locations.
 NOTE: This class is designed to integrate data from multiple drones, but to
@@ -32,20 +34,20 @@ class Kalman:
   def __init__(self, position, velocity, lob, transmitter_position):
     # The transmitter x and y position should technically never change, so it's
     # not part of our state.
-    self.transmitter_position = transmitter_position
+    self.__transmitter_position = transmitter_position
 
     # We need an x and y position, an x and y velocity,
     # and an LOB.
-    self.state = np.array([position[0], position[1], velocity[0], velocity[1],
+    self.__state = np.array([position[0], position[1], velocity[0], velocity[1],
                            lob])
     logger.debug("Initial state: %s" % (self.state))
-    state_size = self.state.shape[0]
+    state_size = self.__state.shape[0]
     # Default observations.
-    self.observations = np.copy(self.state)
+    self.__observations = np.copy(self.state)
 
     # Build a covariance matrix. We will use our initial uncertainty constants,
     # and assume that nothing in the state is correlated with anything else.
-    self.state_covariances = np.zeros((state_size, state_size))
+    self.__state_covariances = np.zeros((state_size, state_size))
     # Calculate our diagonal fill, which will repeat, since the state repeats
     # for each drone.
     diagonal_fill = [self.DRONE_POSITION_UNCERTAINTY,
@@ -54,19 +56,19 @@ class Kalman:
                      self.DRONE_VELOCITY_UNCERTAINTY,
                      self.LOB_UNCERTAINTY]
     diagonal_indices = np.diag_indices(state_size)
-    self.state_covariances[diagonal_indices] = diagonal_fill
+    self.__state_covariances[diagonal_indices] = diagonal_fill
     logger.debug("Initializing state covariances: %s" % \
-                 (self.state_covariances))
+                 (self.__state_covariances))
 
     # Since our initial state is directly what's measured from our sensors, it
     # seems logical that the initial observation covariances would be about what
     # our initial state covariances are.
-    self.observation_covariances = np.copy(self.state_covariances)
+    self.__observation_covariances = np.copy(self.__state_covariances)
 
     # Initialize the Kalman filter.
-    self.kalman = \
+    self.__kalman = \
     AdditiveUnscentedKalmanFilter( \
-        observation_covariance=self.observation_covariances,
+        observation_covariance=self.__observation_covariances,
         transition_functions=self.__transition_function,
         observation_functions=self.__observation_function)
 
@@ -82,8 +84,8 @@ class Kalman:
     new_state[1] += current_state[3]
 
     # We can calculate our LOB too, based on our position.
-    new_state[4] = math.atan2(self.transmitter_position[1] - current_state[1],
-                              self.transmitter_position[0] - current_state[0])
+    new_state[4] = math.atan2(self.__transmitter_position[1] - current_state[1],
+                              self.__transmitter_position[0] - current_state[0])
 
     logger.debug("New state prediction: %s" % (new_state))
     return new_state
@@ -100,15 +102,23 @@ class Kalman:
   veloctiy: How fast we think we're going. (X, Y)
   lob: The LOB on the transmitter. """
   def set_observations(self, position, velocity, lob):
-    self.observations = np.array([position[0], position[1], velocity[0],
+    self.__observations = np.array([position[0], position[1], velocity[0],
                                   velocity[1], lob])
-    logger.debug("Setting new observations: %s" % (self.observations))
+    logger.debug("Setting new observations: %s" % (self.__observations))
 
   """ Updates the filter for one iteration. """
   def update(self):
     logger.info("Updating kalman filter.")
-    output = self.kalman.filter_update(self.state, self.state_covariances,
-                                       observation=self.observations)
-    self.state, self.state_covariances = output
+    output = self.__kalman.filter_update(self.__state, self.__state_covariances,
+                                       observation=self.__observations)
+    self.__state, self.__state_covariances = output
     logger.debug("New state: %s, New state covariance: %s" % \
-                 (self.state, self.state_covariances))
+                 (self.__state, self.__state_covariances))
+
+  """ Returns: The current state. """
+  def state(self):
+    return self.__state
+
+  """ Returns: The current state covariances. """
+  def state_covariances(self):
+    return self.__state_covariances
---
+++ tests/test_kalman.py
@@ -0,0 +1,63 @@
+""" Tests for the Kalman filter. """
+
+
+import unittest
+
+import numpy as np
+
+import kalman
+
+
+""" A superclass for all test cases that defines some useful methods. """
+class _BaseTest(unittest.TestCase):
+  """ Makes sure that a paremeter is within a cetain amount of something else.
+  expected: The value we expected.
+  actual: The value we got.
+  error: The maximum acceptable deviation between expected and actual. """
+  def _assert_near(self, expected, actual, error):
+    self.assertLess(abs(expected - actual), error)
+
+
+""" Tests for the Kalman class. """
+class KalmanTests(_BaseTest):
+  """ Tests that the filter gives reasonable values under extremely basic
+  circumstances. """
+  def test_basic(self):
+    basic_filter = kalman.Kalman((1, 0), (1, 0), 0, (5, 0))
+    basic_filter.set_observations((2, 0), (1, 0), 0)
+    basic_filter.update()
+
+    state = basic_filter.state()
+
+    # We should see values that our quite close to our observation since,
+    # (surprise, surprise) our observation lines up EXACTLY with our model.
+    self._assert_near(2, state[0], 0.01)
+    self._assert_near(0, state[1], 0.01)
+    self._assert_near(1, state[2], 0.01)
+    self._assert_near(0, state[3], 0.01)
+    self._assert_near(0, state[4], 0.01)
+
+    covariances = basic_filter.state_covariances()
+
+    # Our covariances should have gotten close to zero, since our observations
+    # match our model so well.
+    for x in np.nditer(covariances):
+      self._assert_near(0, x, 0.05)
+
+    # Run another perfect iteration.
+    basic_filter.set_observations((3, 0), (1, 0), 0)
+    basic_filter.update()
+
+    state = basic_filter.state()
+
+    self._assert_near(3, state[0], 0.01)
+    self._assert_near(0, state[1], 0.01)
+    self._assert_near(1, state[2], 0.01)
+    self._assert_near(0, state[3], 0.01)
+    self._assert_near(0, state[4], 0.01)
+
+    covariances = basic_filter.state_covariances()
+
+    # Our covariances should be even closer this time.
+    for x in np.nditer(covariances):
+      self._assert_near(0, x, 0.01)
