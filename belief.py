2be20634865a62d653fe7f1e85d0429a6cd618de
Daniel Petti <djpetti@gmail.com>
2015-09-14 20:48:05 -0400

    Make belief classes styleguide-compliant.

--- belief.py
+++ belief.py
@@ -2,15 +2,16 @@
 
 
 """ Contains meta-information about a belief. """
-class BeliefHeader:
+class BeliefHeader(object):
   def __init__(self, **kwargs):
     # Timestamp for the belief in seconds from the epoch.
     self.timestamp = kwargs.get("timestamp")
     # Which drone the belief is coming from.
     self.drone = kwargs.get("drone")
 
+
 """ Represents an Avalanche Transmitter that this drone is in contact with. """
-class AvalancheTransmitter:
+class AvalancheTransmitter(object):
   def __init__(self, **kwargs):
     # A unique identifier for the transmitter.
     self.name = kwargs.get("name")
@@ -19,8 +20,9 @@ class AvalancheTransmitter:
     # Where we currently think the transmitter is, in GPS coordinates. (x, y)
     self.position = kwargs.get("position")
 
+
 """ Contains the actual belief information. """
-class Belief:
+class Belief(object):
   def __init__(self, transmitters=[], **kwargs):
     # The header for this belief.
     self.header = kwargs.get("header")
